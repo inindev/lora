9241267167343afca4bf0228ecb7a1024be5ebd9
John Clark <inindev@gmail.com>
2024-03-28 12:08:35 -0400

    add dechirp method and assorted cleanup
    Signed-off-by: John Clark <inindev@gmail.com>

--- sdr_decode/LoraPhy.m
+++ sdr_decode/LoraPhy.m
@@ -1,12 +1,12 @@
-%
+
 % a copy of: https://github.com/jkadbear/LoRaPHY/blob/master/LoRaPHY.m
 % rewritten to simplify and make it easier for me to read
 %
 
-% phy = LoraPhy(7, 125e3, 1024e3, 'lora_923.3_sample/lora.raw');
-% phy.detect(1, 1);
-% for jj = (1:256); phy.detect(jj, 1); end
-% phy.plot_symbols(1024, 13);
+% phy = LoraPhy(7, 125e3, 'lora_923.3_sample/lora.raw', 1024e3, true)
+% phy.detect(1)
+% for jj = (1:256); phy.detect(jj); end
+% phy.plot_symbols(1024, 13)
 
 classdef LoraPhy < handle & matlab.mixin.Copyable
     properties (SetAccess = immutable)
@@ -30,7 +30,8 @@ classdef LoraPhy < handle & matlab.mixin.Copyable
     end
 
     methods
-        function this = LoraPhy(sf, bw, fs, filename, invert)
+        function this = LoraPhy(sf, bw, filename, file_fs, invert)
+            % LoraPhy
             if(nargin < 5)
                 invert = false;
             end
@@ -48,8 +49,8 @@ classdef LoraPhy < handle & matlab.mixin.Copyable
             this.cfo = 0;             % carrier frequency offset
 
             this.sig = LoraPhy.read(filename);
-            this.sig = lowpass(this.sig, bw/2, fs);
-            this.sig = resample(this.sig, this.fs, fs);  % resample signal @ 2x bandwidth
+            this.sig = lowpass(this.sig, bw/2, file_fs);
+            this.sig = resample(this.sig, this.fs, file_fs);  % resample signal @ 2x bandwidth
             if(invert)
                 % swap I and Q channels
                 this.sig = imag(this.sig) + 1j * real(this.sig);
@@ -57,14 +58,13 @@ classdef LoraPhy < handle & matlab.mixin.Copyable
 
             t = (0:this.sps-1) / this.fs;
             this.upchirp = chirp(t, -this.bw/2, t(end), this.bw/2, 'linear', 0, 'complex').';
+            %close all; pspectrum(this.upchirp, this.fs, "spectrogram", Reassign=true);
             this.downchirp = chirp(t, this.bw/2, t(end), -this.bw/2, 'linear', 0, 'complex').';
-            this.chirps = [this.upchirp, this.downchirp];
-            %close all; pspectrum(this.chirps(:,1), this.fs, "spectrogram", Reassign=true);
         end
 
-        %
-        % dir: 0 = upchirp, 1 = downchirp
-        function x = detect(this, pos, dir)
+        function x = detect(this, pos)
+            % DETECT(pos, cdir)
+            %   pos:    offset from the beginning of signal
             x = 0;
             if(pos < 1)
                 return;
@@ -74,9 +74,7 @@ classdef LoraPhy < handle & matlab.mixin.Copyable
             det_count = 0;
             last = 0;
             while(pos <= (length(this.sig) - this.sps))
-                ft = fft(this.sig(pos:pos+this.sps-1) .* this.chirps(:,dir+1), this.ft_len);
-                ft_pow = abs(ft(1:this.ft_bins)) + abs(ft(this.ft_len - this.ft_bins+1:this.ft_len));
-                [~, fbin] = max(ft_pow);
+                fbin = dechirp(this, pos);
 
                 if(abs(fbin - last) <= this.ft_det_bins)
                     det_count = det_count + 1;
@@ -94,55 +92,74 @@ classdef LoraPhy < handle & matlab.mixin.Copyable
             end
         end
 
-        %
-        % usage: plot_symbols(this, pos, [sym_count])
-        %
-        %  create upchirped / downcirped plots of the signal starting at
-        %    'pos' for 'sym_count' symbols
-        %  note: the max fft power value is indicated by a red diamond
-        %
+        function [fbin, mval, ft_pow] = dechirp(this, pos, is_up)
+            % DECHIRP:  apply dechirping on the symbol at pos
+            % input:
+            %      pos:  symbol index
+            %    is_up:  apply upchirp rather than downchirp
+            % output:
+            %     fbin:  fft bin with the greatest power
+            %     mval:  maximum fft power value
+            %   ft_pow:  vector of power levels by bin
+            if(nargin < 3)
+                is_up = false;
+            end
+
+            if(is_up)
+                chp = this.upchirp;
+            else
+                chp = this.downchirp;
+            end
+
+            ft = fft(this.sig(pos:pos+this.sps-1) .* chp, this.ft_len);
+            ft_pow = abs(ft(1:this.ft_bins)) + abs(ft(this.ft_len - this.ft_bins+1:this.ft_len));
+            [mval, fbin] = max(ft_pow);
+        end
+
         function plot_symbols(this, pos, sym_count)
-            if(nargin < 2)
+            % PLOT_SYMBOLS(this, pos, [sym_count])
+            %   pos:        offset from the beginning of signal
+            %   sym_count:  the number of symbols to plot (default 9)
+            %
+            %   create upchirped / downcirped plots of the signal starting at
+            %    'pos' for 'sym_count' symbols
+            %   note: the max fft power value is indicated by a red diamond
+            if(nargin < 3)
                 sym_count = 9;
             end
 
             type = ["upchirped", "dnchirped"];
 
-            figure;
+            f = figure(1);
+            scn = get(0,'screensize');
+            f.Position = [1,439,scn(1,3)-120,680];
 
             for jj = 1:sym_count
-                for dir = 0:1
+                for cdir = 1:2
                     % 2 x n plot
-                    ft = fft(this.sig(pos:pos+this.sps-1) .* this.chirps(:,dir+1), this.ft_len);
-                    ft_pow = abs(ft(1:this.ft_bins)) + abs(ft(this.ft_len - this.ft_bins+1:this.ft_len));
-
-                    s = subplot(2, sym_count, dir*sym_count + jj);
+                    [fbin, mval, ft_pow] = this.dechirp(pos, (cdir==1));
+                    subplot(2, sym_count, ((cdir-1) * sym_count + jj));
                     plot(ft_pow);
                     %hist(ft_pow, length(ft_pow));
-                    title(type(dir+1) + " " + jj);
+                    title(type(cdir) + " " + jj);
 
                     hold on
-                    [mval, fbin] = max(ft_pow);
                     plot(fbin, mval, 'rd');
-                    %fprintf("plot(%d,%d) fbin = %d\n", dir+1, jj, fbin);
+                    %fprintf("plot(%d,%d) fbin = %d\n", cdir, jj, fbin);
                 end
 
                 pos = pos + this.sps;
             end
         end
-
     end
 
 
     methods(Static)
-        %
-        % usage: read(filename, [count])
-        %
-        %  open filename and return the contents as a column vector,
-        %  treating them as 32 bit complex numbers
-        %
-        % https://github.com/gnuradio/gnuradio/blob/master/gr-utils/octave/read_complex_binary.m
         function v = read(filename, count)
+            % READ(filename, [count])
+            %   open filename and return the contents as a column vector,
+            %   treating them as 32 bit complex numbers
+            %   https://github.com/gnuradio/gnuradio/blob/master/gr-utils/octave/read_complex_binary.m
             narginchk(1, 2);
 
             if(nargin < 2)
@@ -161,16 +178,12 @@ classdef LoraPhy < handle & matlab.mixin.Copyable
             end
         end
 
-        %
-        % usage: write(data, filename)
-        %
-        %  open filename and write data to it
-        %  Format is interleaved float IQ e.g. each
-        %  I,Q 32-bit float IQIQIQ....
-        %  This is compatible with read()
-        %
-        % https://github.com/gnuradio/gnuradio/blob/master/gr-utils/octave/write_complex_binary.m
-        function v = write(data, filename)
+        function v = write(filename, data)
+            % WRITE(filename, data)
+            %   write IQ data to 'filename'
+            %   data format is interleaved 32-bit IQ float values: IQIQIQ....
+            %   note: this is compatible with READ()
+            %   https://github.com/gnuradio/gnuradio/blob/master/gr-utils/octave/write_complex_binary.m
             narginchk(2, 2);
 
             f = fopen(filename, 'wb');
